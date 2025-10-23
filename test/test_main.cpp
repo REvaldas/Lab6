7476353458e90a3d653f6833287ca57100e58153
REvaldas <122756600+REvaldas@users.noreply.github.com>
2025-10-23 20:46:16 +0300

    Update test_main.cpp

--- test/test_main.cpp
+++ test/test_main.cpp
@@ -1,20 +1,17 @@
 #include <cassert>
-#include "../src/main.cpp"
 #include <iostream>
+#include "../src/BankAccount.h"
 
 int main() {
     BankAccount account(100.0);
 
-    // Test deposit
     account.deposit(50);
     assert(account.getBalance() == 150);
 
-    // Test withdraw success
     bool success = account.withdraw(70);
     assert(success);
     assert(account.getBalance() == 80);
 
-    // Test withdraw failure
     success = account.withdraw(200);
     assert(!success);
     assert(account.getBalance() == 80);
